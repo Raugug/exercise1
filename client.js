2e58314dac63d54ab04bcc35b66626befc01cef6
RaulG <guiaraul@gmail.com>
2018-10-29 17:34:07 +0100

    VALIDATIONS + CLIENT SERVICE

---
+++ client.js
@@ -0,0 +1,10 @@
+const ClientService = require('./ClientService');
+
+
+const client = new ClientService();
+console.log(client.send_message("", "cosas"));
+console.log(client.send_message("", "cosas"));
+console.log(client.send_message("", "cosas"));
+console.log(client.send_message("", "cosas"));
+console.log(client.send_message("weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", "cosas"));
+console.log(client.send_message("", "cosas"));
--- index.js
+++ index.js
@@ -3,31 +3,48 @@ const bodyParser = require('body-parser');
 var axios = require('axios')
 var app = express();
 
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((err, req, res, next) =>{
+  
+  if (err instanceof SyntaxError) {
+    res.status(400).json({status: "INVALID JSON FORMAT"})
+  } else {
+    next();
+  }
+
+})
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.get('/message', (req, res, next)=> {
-  axios.get('http://messageapp:3000/message')
-  .then(response => {
-    console.log("GET OK", response.data);
-    res.status(200).json({
-      status: "OK",
-      response: response.data
-    });
-  })
-  .catch(err => {
-    console.log(err)
-    res.status(500).json({status: "SERVER GET ERROR"})
-  })
-})
 
 app.post('/message', (req,res, next) => {
-  let destination = req.body.destination;
-  let body = req.body.body;
+
+  
+  const {destination, body} = req.body
+  if(!destination || !body) {
+    console.log("body or destination not provided")
+    res.status(400).json({status: "body or destination not provided"})
+    return;
+  }
+  if (typeof destination !== "string" || typeof body !== "string"){
+      console.log("incorrect type of parameters")
+    res.status(400).json({status: "body and destination must be strings"})
+    return;
+  }
+  if (destination.length == 0 || body.length == 0) {
+    console.log("fields must be filled")
+    res.status(400).json({status: "all fields must be filled"})
+    return;
+  }
+
+
   axios.post('http://messageapp:3000/message', {destination, body})
   .then(response => {
     console.log("POST OK", response.data);
@@ -38,7 +55,7 @@ app.post('/message', (req,res, next) => {
   })
   .catch(err => {
     console.log(err)
-    res.status(500).json({status: "SERVER POST ERROR"})
+    res.status(500).json({status: "SERVER ERROR: EXTERNAL SERVICE DIDN'T RESPONSE"})
   })
 
 })
